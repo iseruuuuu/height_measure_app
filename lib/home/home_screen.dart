ccc80e9c97d7a5eb971c0d26e98e97b1f8ad143a
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-07 22:05:14 +0900

    add: 座標を取得するものを作成

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,20 +1,59 @@
+/*
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Popup Demo'),
+        ),
+        body: new MyWidget());
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyWidgetState();
+  }
+}
+
+class MyWidgetState extends State<MyWidget> {
+  double posx = 100.0;
+  double posy = 100.0;
+
+  void onTapDown(BuildContext context, TapDownDetails details) {
+    print('${details.globalPosition}');
+    final RenderObject? box = context.findRenderObject();
+    final Offset localOffset = box.globalToLocal(details.globalPosition);
+    setState(() {
+      posx = localOffset.dx;
+      posy = localOffset.dy;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('あなたの身長'),
-            Text('160cm'),
-          ],
-        ),
-      ),
+    return new GestureDetector(
+      onTapDown: (TapDownDetails details) => onTapDown(context, details),
+      child: new Stack(fit: StackFit.expand, children: <Widget>[
+        // Hack to expand stack to fill all the space. There must be a better
+        // way to do it.
+        new Container(color: Colors.white),
+        new Positioned(
+          child: new Text('hello'),
+          left: posx,
+          top: posy,
+        )
+      ]),
     );
   }
 }
+
+ */
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-
-import 'home/home_screen.dart';
-
+import 'package:geolocator/geolocator.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,8 +11,58 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
-
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  String _location = "no data";
+  Future<void> getLocation() async {
+    // 現在の位置を返す
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    // 北緯がプラス。南緯がマイナス
+    print("緯度: " + position.latitude.toString());
+    // 東経がプラス、西経がマイナス
+    print("経度: " + position.longitude.toString());
+    // 高度
+    print("高度: " + position.altitude.toString());
+    // 距離をメートルで返す
+    double distanceInMeters =
+    Geolocator.distanceBetween(35.68, 139.76, -23.61, -46.40);
+    print(distanceInMeters);
+    // 方位を返す
+    double bearing = Geolocator.bearingBetween(35.68, 139.76, -23.61, -46.40);
+    print(bearing);
+    setState(() {
+      _location = position.toString();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title!),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_location',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: getLocation, child: Icon(Icons.location_on)),
     );
   }
 }
