914d495b5f5645b720ba3aae099778f3bb40356d
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-09 14:15:51 +0900

    add :homeに追加した

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:math_expressions/math_expressions.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
+import 'home_screen_state.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -10,190 +11,90 @@ class Home extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
 class HomeScreen extends StatefulWidget {
-  HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  _HomeState createState() => _HomeState();
 }
-class _HomeScreenState extends State<HomeScreen> {
 
-  String lati = '';
-  String long = '';
-  String alti = '';
-  String dist = '';
-  String bear = '';
-
-  String lati2 = '';
-  String long2 = '';
-  String alti2 = '';
-  String dist2 = '';
-  String bear2 = '';
-
-  String lati4 = '';
-  String long4 = '';
-  String alti4 = '';
-  String dist4 = '';
-  String bear4 = '';
-
-  String lati6 = '';
-  String long6 = '';
-  String alti6 = '';
-  String dist6 = '';
-  String bear6 = '';
-
-  bool change = false;
-  bool change2 = false;
-
-
-  Future<void> getLocation() async {
-    if(change == false) {
-      // 現在の位置を返す
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      // 北緯がプラス。南緯がマイナス
-      //print("緯度: " + position.latitude.toString());
-      // 東経がプラス、西経がマイナス
-      //print("経度: " + position.longitude.toString());
-      // 高度
-      //print("高度: " + position.altitude.toString());
-      // 距離をメートルで返す
-      double distanceInMeters = Geolocator.distanceBetween(35.68, 139.76, -23.61, -46.40);
-      //print(distanceInMeters);
-      // 方位を返す
-      double bearing = Geolocator.bearingBetween(35.68, 139.76, -23.61, -46.40);
-      //print(bearing);
-      setState(() {
-        //緯度
-        lati = position.latitude.toString();
-        //経度
-        long = position.longitude.toString();
-        //高度
-        alti = position.altitude.toString();
-        //距離をメートルで返す
-        dist = '$distanceInMeters';
-        //方位
-        bear = '$bearing';
-      });
-    }else{
-      setState(() {
-        lati2 = lati;
-        long2 = long;
-        alti2 = alti;
-        dist2 = dist;
-        bear2 = bear;
-      });
-    }
-
-    if(change2 == false) {
-
-    }else{
-      setState(() {
-        lati4 = lati + '-' + lati2;
-        long4 = long + '-' + long2;
-        alti4 = alti + '-' + alti2;
-        dist4 = dist + '-' + dist2;
-        bear4 = bear + '-' + bear2;
-
-        Parser p = Parser();
-        Expression exp = p.parse(lati4);
-        Expression exp2 = p.parse(long4);
-        Expression exp3 = p.parse(alti4);
-        Expression exp4 = p.parse(dist4);
-        Expression exp5 = p.parse(bear4);
-        ContextModel cm = ContextModel();
-
-        lati6 = exp.evaluate(EvaluationType.REAL, cm).toString();
-        long6 = exp2.evaluate(EvaluationType.REAL, cm).toString();
-        alti6 = exp3.evaluate(EvaluationType.REAL, cm).toString();
-        dist6 = exp4.evaluate(EvaluationType.REAL, cm).toString();
-        bear6 = exp5.evaluate(EvaluationType.REAL, cm).toString();
-
-        print(lati6);
-        print(long6);
-        print(alti6);
-        print(dist6);
-        print(bear6);
-      });
-    }
-  }
-
-  void tap() {
-    change = !change;
-    print(change);
-  }
-
-  void tap2() {
-    change2 = !change2;
-    print(change2);
-  }
-
+class _HomeState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home_Screen'),
+    return StateNotifierProvider<HomeScreenController, HomeScreenState>(
+      create: (context) => HomeScreenController(
+        context: context,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("緯度: " + lati),
-            Text("経度: " + long),
-            Text("高度: " + alti),
-            Text("距離: " + dist),
-            Text("方角: " + bear),
-
-            const SizedBox(height: 50),
-
-            Text("緯度: " + lati2),
-            Text("経度: " + long2),
-            Text("高度: " + alti2),
-            Text("距離: " + dist2),
-            Text("方角: " + bear2),
-
-            const SizedBox(height: 50),
-
-            Text("緯度: " + lati6),
-            Text("経度: " + long6),
-            Text("高度: " + alti6),
-            Text("距離: " + dist6),
-            Text("方角: " + bear6),
-          ],
-        ),
-      ),
-      floatingActionButton: Column(
-        verticalDirection: VerticalDirection.up, // childrenの先頭を下に配置
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          FloatingActionButton(
-            backgroundColor: Colors.redAccent,
-            onPressed: () {
-              tap();
-            },
-            child: const Text('1'),
-          ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 16.0),
-            child:  FloatingActionButton(
-              onPressed: getLocation,
-              child: const Icon(Icons.location_on),
+      builder: (context, _) {
+        final latitude = context.select<HomeScreenState, String>((state) => state.latitude);
+        final longitude = context.select<HomeScreenState, String>((state) => state.longitude);
+        final altitude = context.select<HomeScreenState, String>((state) => state.altitude);
+        final distanceInMeters = context.select<HomeScreenState, String>((state) => state.distanceInMeters);
+        final bearing = context.select<HomeScreenState, String>((state) => state.bearing);
+        return GestureDetector(
+          onTap: () => context.read<HomeScreenController>().initState(),
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              elevation: 3,
+              backgroundColor: Colors.white,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: IconButton(
+                  onPressed: () => context.read<HomeScreenController>().tap2(),
+                  icon: const Icon(
+                    Icons.account_circle,
+                    color: Colors.blueAccent,
+                    size: 40,
+                  ),
+                ),
+              ),
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: IconButton(
+                    onPressed: () => context.read<HomeScreenController>().tap(),
+                    icon: const Icon(
+                      Icons.contact_support_rounded,
+                      color: Colors.blueAccent,
+                      size: 40,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('緯度は、' + latitude),
+                  Text('経度は、' + longitude),
+                  Text('高度は、' + altitude),
+                  Text('距離は、' + distanceInMeters),
+                  Text('方角は、' + bearing),
+                  ElevatedButton(
+                    onPressed: () => context.read<HomeScreenController>().getLocation(),
+                    child: const Text('①初めの値を設定する。'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => context.read<HomeScreenController>().tap(),
+                    child: const Text('②'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => context.read<HomeScreenController>().tap2(),
+                    child: const Text('③'),
+                  ),
+                ],
+              ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 16.0),
-            child:  FloatingActionButton(
-              onPressed: () {
-                tap2();
-              },
-              child: const Text('2'),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/home/home_screen_state.dart
+++ lib/home/home_screen_state.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:state_notifier/state_notifier.dart';
 
+import 'package:geolocator/geolocator.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 part 'home_screen_state.freezed.dart';
 
@@ -24,10 +26,116 @@ class HomeScreenController extends StateNotifier<HomeScreenState> with LocatorMi
 
   final BuildContext context;
 
+  String lati = '';
+  String long = '';
+  String alti = '';
+  String dist = '';
+  String bear = '';
+
+  String lati2 = '';
+  String long2 = '';
+  String alti2 = '';
+  String dist2 = '';
+  String bear2 = '';
+
+  String lati4 = '';
+  String long4 = '';
+  String alti4 = '';
+  String dist4 = '';
+  String bear4 = '';
+
+  String lati6 = '';
+  String long6 = '';
+  String alti6 = '';
+  String dist6 = '';
+  String bear6 = '';
+
+  bool change = false;
+  bool change2 = false;
+
   @override
   void initState() {
     super.initState();
 
-
   }
+
+  Future<void> getLocation() async {
+    if(change == false) {
+      // 現在の位置を返す
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      // 北緯がプラス。南緯がマイナス
+      //print("緯度: " + position.latitude.toString());
+      // 東経がプラス、西経がマイナス
+      //print("経度: " + position.longitude.toString());
+      // 高度
+      //print("高度: " + position.altitude.toString());
+      // 距離をメートルで返す
+      double distanceInMeters = Geolocator.distanceBetween(35.68, 139.76, -23.61, -46.40);
+      //print(distanceInMeters);
+      // 方位を返す
+      double bearing = Geolocator.bearingBetween(35.68, 139.76, -23.61, -46.40);
+      //print(bearing);
+      //緯度
+      lati = position.latitude.toString();
+      //経度
+      long = position.longitude.toString();
+      //高度
+      alti = position.altitude.toString();
+      //距離をメートルで返す
+      dist = '$distanceInMeters';
+      //方位
+      bear = '$bearing';
+    }else{
+      lati2 = lati;
+      long2 = long;
+      alti2 = alti;
+      dist2 = dist;
+      bear2 = bear;
+    }
+
+    if(change2 == false) {
+    }else{
+      lati4 = lati + '-' + lati2;
+      long4 = long + '-' + long2;
+      alti4 = alti + '-' + alti2;
+      dist4 = dist + '-' + dist2;
+      bear4 = bear + '-' + bear2;
+
+      Parser p = Parser();
+      Expression exp = p.parse(lati4);
+      Expression exp2 = p.parse(long4);
+      Expression exp3 = p.parse(alti4);
+      Expression exp4 = p.parse(dist4);
+      Expression exp5 = p.parse(bear4);
+      ContextModel cm = ContextModel();
+
+      lati6 = exp.evaluate(EvaluationType.REAL, cm).toString();
+      long6 = exp2.evaluate(EvaluationType.REAL, cm).toString();
+      alti6 = exp3.evaluate(EvaluationType.REAL, cm).toString();
+      dist6 = exp4.evaluate(EvaluationType.REAL, cm).toString();
+      bear6 = exp5.evaluate(EvaluationType.REAL, cm).toString();
+
+      state = state.copyWith(
+        latitude: lati6,
+        longitude: long6,
+        altitude: alti6,
+        distanceInMeters: dist6,
+        bearing: bear6,
+      );
+
+
+    }
+  }
+
+  void tap() {
+    change = !change;
+    print(change);
+  }
+
+  void tap2() {
+    change2 = !change2;
+    print(change2);
+  }
+
+
 }
