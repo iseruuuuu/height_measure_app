e64ebff10bbf574f416a9800c85d01746578ccdd
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-08-09 13:42:27 +0900

    add: home画面を作成

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
+import 'package:math_expressions/math_expressions.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -10,17 +10,17 @@ class Home extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 class HomeScreen extends StatefulWidget {
-  HomeScreen({Key? key, this.title}) : super(key: key);
-  final String? title;
+  HomeScreen({Key? key}) : super(key: key);
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 class _HomeScreenState extends State<HomeScreen> {
+
   String _location = "no data";
 
   String lati = '';
@@ -35,7 +35,20 @@ class _HomeScreenState extends State<HomeScreen> {
   String dist2 = '';
   String bear2 = '';
 
+  String lati4 = '';
+  String long4 = '';
+  String alti4 = '';
+  String dist4 = '';
+  String bear4 = '';
+
+  String lati6 = '';
+  String long6 = '';
+  String alti6 = '';
+  String dist6 = '';
+  String bear6 = '';
+
   bool change = false;
+  bool change2 = false;
 
 
   Future<void> getLocation() async {
@@ -49,8 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
       // 高度
       //print("高度: " + position.altitude.toString());
       // 距離をメートルで返す
-      double distanceInMeters =
-      Geolocator.distanceBetween(35.68, 139.76, -23.61, -46.40);
+      double distanceInMeters = Geolocator.distanceBetween(35.68, 139.76, -23.61, -46.40);
       //print(distanceInMeters);
       // 方位を返す
       double bearing = Geolocator.bearingBetween(35.68, 139.76, -23.61, -46.40);
@@ -77,11 +89,48 @@ class _HomeScreenState extends State<HomeScreen> {
         bear2 = bear;
       });
     }
+
+    if(change2 == false) {
+
+    }else{
+      setState(() {
+        lati4 = lati + '-' + lati2;
+        long4 = long + '-' + long2;
+        alti4 = alti + '-' + alti2;
+        dist4 = dist + '-' + dist2;
+        bear4 = bear + '-' + bear2;
+
+        Parser p = Parser();
+        Expression exp = p.parse(lati4);
+        Expression exp2 = p.parse(long4);
+        Expression exp3 = p.parse(alti4);
+        Expression exp4 = p.parse(dist4);
+        Expression exp5 = p.parse(bear4);
+        ContextModel cm = ContextModel();
+
+        lati6 = exp.evaluate(EvaluationType.REAL, cm).toString();
+        long6 = exp2.evaluate(EvaluationType.REAL, cm).toString();
+        alti6 = exp3.evaluate(EvaluationType.REAL, cm).toString();
+        dist6 = exp4.evaluate(EvaluationType.REAL, cm).toString();
+        bear6 = exp5.evaluate(EvaluationType.REAL, cm).toString();
+
+        print(lati6);
+        print(long6);
+        print(alti6);
+        print(dist6);
+        print(bear6);
+      });
+    }
   }
 
   void tap() {
     change = !change;
     print(change);
+  }
+
+  void tap2() {
+    change2 = !change2;
+    print(change2);
   }
 
 
@@ -98,7 +147,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Text("距離: " + dist),
             Text("方角: " + bear),
 
-            const SizedBox(height: 30),
+            const SizedBox(height: 50),
 
             Text("緯度: " + lati2),
             Text("経度: " + long2),
@@ -106,8 +155,13 @@ class _HomeScreenState extends State<HomeScreen> {
             Text("距離: " + dist2),
             Text("方角: " + bear2),
 
+            const SizedBox(height: 50),
 
-
+            Text("緯度: " + lati6),
+            Text("経度: " + long6),
+            Text("高度: " + alti6),
+            Text("距離: " + dist6),
+            Text("方角: " + bear6),
           ],
         ),
       ),
@@ -120,13 +174,22 @@ class _HomeScreenState extends State<HomeScreen> {
             onPressed: () {
               tap();
             },
-            child: const Icon(Icons.extension),
+            child: const Text('1'),
           ),
-          Container( // 余白のためContainerでラップ
+          Container(
             margin: const EdgeInsets.only(bottom: 16.0),
             child:  FloatingActionButton(
               onPressed: getLocation,
               child: const Icon(Icons.location_on),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 16.0),
+            child:  FloatingActionButton(
+              onPressed: () {
+                tap2();
+              },
+              child: const Text('2'),
             ),
           ),
         ],
